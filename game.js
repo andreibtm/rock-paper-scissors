fe6eb0766515139f613066120f5203e47cb0af04
andreibtm <vanvuandrei676@gmail.com>
2024-05-05 17:52:16 +0300

    Added and completed the playRound function

--- game.js
+++ game.js
@@ -1,22 +1,50 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
-    const randomNumber = Math.random();
+  const randomNumber = Math.random();
 
-    if (randomNumber < 0.33) {
-        choice = "rock";
-      } else if (randomNumber < 0.66) {
-        choice = "paper";
-      } else {
-        choice = "scissors";
-      }
-    
-      return choice;
+  if (randomNumber < 0.33) {
+      choice = "rock";
+    } else if (randomNumber < 0.66) {
+      choice = "paper";
+    } else {
+      choice = "scissors";
+    }
+  
+    return choice;
 }
 
 function getHumanChoice() {
-    let humanChoice = prompt("Chosse between rock, paper or scissors!")
+  let humanChoice = prompt("Chosse between rock, paper or scissors!")
+  const choiceList = ["rock", "paper", "scissors"];
 
-    return humanChoice;
+  while(choiceList.indexOf(humanChoice) === -1 ){
+    humanChoice = prompt("Type a valid choice! Chosse between rock, paper or scissors!")
+  }
+
+  return humanChoice;
+}
+
+function playRound(humanChoice, computerChoice) {
+  // Compare choices and determine winner
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie!")
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    // Player wins
+    humanScore++;
+    console.log(`You win! ${capitalizeFirstLetter(humanChoice)} beats ${capitalizeFirstLetter(computerChoice)}`);
+  } else {
+    // Computer wins
+    computerScore++;
+    console.log(`You lose! ${capitalizeFirstLetter(computerChoice)} beats ${capitalizeFirstLetter(humanChoice)}`);
+  }
+}
+
+const humanSelection = getHumanChoice().toLowerCase();
+const computerSelection = getComputerChoice().toLowerCase();
+
+function capitalizeFirstLetter(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
